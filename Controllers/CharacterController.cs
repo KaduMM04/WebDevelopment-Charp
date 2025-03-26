1ce586e81b98cfa5bdef9d2dd522ebcd173c372f
CARLOS EDUARDO MENDES MIRANDA <carlos.miranda@cs.up.edu.br>
2025-03-26 20:06:57 -0300

    create readonly _appDbContext

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjetoDBZ.Data;
 
 namespace ProjetoDBZ.Controllers 
 {
     [ApiController]
     [Route ("api/[controller]")]
-    public class CharacterController : ControllerBase {
+    public class CharacterController : ControllerBase 
+    {
+        private readonly AppDbContext _appDbContext;
 
+        public CharacterController(AppDbContext appDbContext) {
+            _appDbContext = appDbContext;
+        }
     }
 }
