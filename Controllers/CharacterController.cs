fcc6d3807cc31e6706277026263edc23f862f0f6
CARLOS EDUARDO MENDES MIRANDA <carlos.miranda@cs.up.edu.br>
2025-03-26 19:16:23 -0300

    Project and struct create

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoDBZ.Controllers 
+{
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class CharacterController : ControllerBase {
+
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,8 @@
+namespace ProjetoDBZ.Models
+{
+    public class Character {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+    }
+}
